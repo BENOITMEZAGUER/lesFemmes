419b64957ed677cfba8cd71149595d3560ba4cad
BENOITMEZAGUER <benoit.mezaguer@gmail.com>
2024-04-02 11:27:05 +0200

    premier commit

---
+++ lesFemmes/src/App.jsx
@@ -0,0 +1,61 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const womenList = [
+    {
+    firstName:"Margot",
+    lastName: "Robbie",
+    birthDate: "02/07/1990",
+    age: 33,
+    nationality: "australienne",
+    maritalStatus: "married",
+    hobbies: "yoga, dancing, boxe, pilates",
+    resume:"Le loup de' Wall street, Barbie, Suicide Squad, Babylon...",
+    trailer: "https://www.youtube.com/embed/4ZCGpepWUlE?si=RpBsUpH7tJDTE9Vl"
+    };
+
+    /*{
+      firstName:
+      lastName: 
+      birthDate: 
+      age:
+      nationality:
+      maritalStatus:
+      hobbies:
+      resume:
+      trailer:
+      };
+
+      {
+        firstName:
+        lastName: 
+        birthDate: 
+        age:
+        nationality:
+        maritalStatus:
+        hobbies:
+        resume:
+        };
+
+        {
+          firstName:
+          lastName: 
+          birthDate: 
+          age:
+          nationality:
+          maritalStatus:
+          hobbies:
+          resume:
+          } */
+  ]
+
+  return (
+    <>
+
+    
+    </>
+  )
+}
+
+export default App
