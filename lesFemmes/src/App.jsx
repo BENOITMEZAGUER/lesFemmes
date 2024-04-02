7c70e68ee085f7d6cb329a0e89e4b87f5d292510
BENOITMEZAGUER <benoit.mezaguer@gmail.com>
2024-04-02 16:55:09 +0200

    second commit

--- lesFemmes/src/App.jsx
+++ lesFemmes/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import Cards from "./components/Cards";
+import CardsList from "./components/CardsList";
 import "./App.css";
 
 function App() {
@@ -7,53 +7,58 @@ function App() {
     {
       firstName: "Margot",
       lastName: "Robbie",
+      image: "../../assets/images/Margot.jpeg",
       birthDate: "02/07/1990",
       age: 33,
       nationality: "australienne",
-      maritalStatus: "married",
-      hobbies: "yoga, dancing, boxe, pilates",
-      resume: "Le loup de' Wall street, Barbie, Suicide Squad, Babylon...",
+      maritalStatus: "mariée",
+      hobbies: "yoga, dance, boxe, pilates",
+      resume: "Le loup de Wall street, Barbie, Suicide Squad, Babylon...",
       trailer: "https://www.youtube.com/embed/4ZCGpepWUlE?si=RpBsUpH7tJDTE9Vl",
     },
 
-    /*{
-      firstName:
-      lastName: 
-      birthDate: 
-      age:
-      nationality:
-      maritalStatus:
-      hobbies:
-      resume:
-      trailer:
-      };
+    {
+      firstName: "Elizabeth",
+      lastName: "Olsen",
+      image: "../../assets/images/Elizabeth.jpeg",
+      birthDate: "16/02/1989",
+      age: 35,
+      nationality: "américaine",
+      maritalStatus: "mariée",
+      hobbies: "Cuisine, ménage",
+      resume: "Wanda Vision, Love and death, Godzilla...",
+      trailer: "https://www.youtube.com/embed/sj9J2ecsSpo?si=1ltvUnNu2Km5ebk6",
+    },
 
-      {
-        firstName:
-        lastName: 
-        birthDate: 
-        age:
-        nationality:
-        maritalStatus:
-        hobbies:
-        resume:
-        };
+    {
+      firstName: "Dua",
+      lastName: "Lipa",
+      image: "../../assets/images/DuaLipa.jpeg",
+      birthDate: "22/08/1995",
+      age: 28,
+      nationality: "albano-britannique",
+      maritalStatus: "couple",
+      hobbies: "boxe, shopping, voyages",
+      resume: "New Rule, Levitating, Don't start now...",
+    },
 
-        {
-          firstName:
-          lastName: 
-          birthDate: 
-          age:
-          nationality:
-          maritalStatus:
-          hobbies:
-          resume:
-          } */
+    {
+      firstName: "Stefanie",
+      lastName: "Joosten",
+      image: "../../assets/images/Stefanie.jpeg",
+      birthDate: "05/08/1988",
+      age: 36,
+      nationality: "hollandaise",
+      maritalStatus: "célibataire",
+      hobbies: "jeux-vidéos, musqiue, voyages",
+      resume: "Metal Gear Solid V, Transit 17, Hostage X...",
+      trailer: "https://www.youtube.com/embed/JZDBfmD-ckg?si=EfdoyQ575r40oNIZ",
+    },
   ];
 
   return (
     <>
-      <Cards women={womenList} />
+      <CardsList womenList={womenList} />
     </>
   );
 }
--- lesFemmes/src/components/Cards.jsx
+++ lesFemmes/src/components/Cards.jsx
@@ -1,10 +1,31 @@
-function Cards(women) {
+import "./style.scss";
+
+function Cards({ women }) {
   return (
-    <div>
+    <div className="card">
       <h2>
-        {women.firstName}
-        {women.lastName}
+        {women.firstName} {women.lastName}
       </h2>
+
+      <section className="infoCard">
+        <img src={women.image}></img>
+        <div className="info">
+          <p>Date de naissance: {women.birthDate}</p>
+          <p>Âge: {women.age} ans</p>
+          <p>Nationalité: {women.nationality}</p>
+          <p>Situation: {women.maritalStatus}</p>
+          <p>Passions: {women.hobbies}</p>
+          <p>Succès: {women.resume}</p>
+        </div>
+      </section>
+      <div className="youtube">
+        <iframe
+          width="580"
+          height="360"
+          src={women.trailer}
+          allowFullScreen
+        ></iframe>
+      </div>
     </div>
   );
 }
