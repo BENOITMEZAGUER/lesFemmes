3ba0593ef982ea257a132bd25e605f75cdacabe7
AUDREY BAUDRY <baudry.audrey@outlook.fr>
2024-04-03 12:04:12 +0200

    map

---
+++ lesFemmes/src/components/CardsList.jsx
@@ -0,0 +1,13 @@
+import Cards from "./Cards";
+
+function CardsList({ womenList }) {
+  return (
+    <>
+      {womenList.map((person) => (
+        <Cards women={person} />
+      ))}
+    </>
+  );
+}
+
+export default CardsList;
---
+++ lesFemmes/src/components/NavBar.jsx
@@ -0,0 +1,12 @@
+import Search from "./Search";
+
+function NavBar() {
+  return (
+    <div className="navBar">
+      <a href="#actrices">Les actrices</a>
+      <a href="#chanteuses">Les chanteuses</a>
+    </div>
+  );
+}
+
+export default NavBar;
